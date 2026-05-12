1a2a7b3c0011303e84d91e6da76d044d995486e2
Andreas Stefl <stefl.andreas@gmail.com>
2026-05-12 22:45:54 +0200

    try again

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-install-apt python3-pip
-pip3 install conan
+which conan || true
+conan --version || true
+
+install-apt pipx
+pipx ensurepath
+pipx install conan
 
 git clone https://github.com/opendocument-app/conan-odr-index.git
 python conan-odr-index/scripts/conan_export_all_packages.py
