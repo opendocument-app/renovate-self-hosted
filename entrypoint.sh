f114cfb28a2efa20ba35e5d08403c1ba1b4c07f7
Andreas Stefl <stefl.andreas@gmail.com>
2026-05-12 22:54:29 +0200

    again

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,11 @@
 set -e
 
 install-apt python3 pipx
-pipx ensurepath
-pipx install conan
 
-git clone https://github.com/opendocument-app/conan-odr-index.git
-python3 conan-odr-index/scripts/conan_export_all_packages.py
+runuser -u ubuntu pipx ensurepath
+runuser -u ubuntu pipx install conan
+
+runuser -u ubuntu git clone https://github.com/opendocument-app/conan-odr-index.git
+runuser -u ubuntu python3 conan-odr-index/scripts/conan_export_all_packages.py
 
 exec runuser -u ubuntu renovate
